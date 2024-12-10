bba08d299d9d2acb035c66c91e2e8e41d495a21f
tomas3603 <tkhossa@emory.edu>
2024-12-09 19:45:38 -0500

    update readme

--- README.md
+++ README.md
@@ -7,9 +7,26 @@ jupyter notebook
 quarto
 wbdata
 postgresSQL
-
+matplotlib
+seaborn
 
 instructions to reproduce
 1. install dependencies
 2. in command line run (assuming in root directory ...\qtm350-finalproject-group1):
 python scripts/top20countr
+
+
+order in which to run scripts:
+- get_gdp_growth_data.py
+- describe_growth.sql
+- gdp_growth_hist.py
+- gdp_growth_linplot.py
+- get_edu_data.py
+- expenditure_pct_gdp.sql
+- create_expenditure.py
+- create_avg_enrollment_data.py
+- create_tertiary_avg_enrollment.py
+- visualizing_edu_growth.py
+- create_avg_full_data.py
+- correlation.py
+- regressionplot.py
--- scripts/regressionplot.py
+++ scripts/regressionplot.py
@@ -4,20 +4,19 @@ import seaborn as sns
 from sklearn.linear_model import LinearRegression
 import numpy as np
 
-df = pd.read_csv("averaged_full.csv")
+tertiary = pd.read_csv("data/tertiary_enrollment.csv")
+
+df = pd.read_csv("data/averaged_full.csv")
 y = df['avg_gdp_growth']
 x = df['avg_tertiary_enrollment_growth']
-# Reshape x to be a 2D array
+
 x = x.values.reshape(-1, 1)
 
-# Create and fit the model
 model = LinearRegression()
 model.fit(x, y)
 
-# Predict y values
 y_pred = model.predict(x)
 
-# Plot the data and the regression line
 plt.figure(figsize=(10, 6))
 sns.scatterplot(x=x.flatten(), y=y, label='Data')
 plt.plot(x, y_pred, color='red', label='Regression Line')
@@ -25,4 +24,5 @@ plt.xlabel('Average Tertiary Enrollment Growth')
 plt.ylabel('Average GDP Growth')
 plt.title('Linear Regression of GDP Growth on Tertiary Enrollment Growth')
 plt.legend()
+plt.savefig('figures/regression_plot.png')
 plt.show()
